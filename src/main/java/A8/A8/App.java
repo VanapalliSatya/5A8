4a0c949bb5dcad01589c5e7c183db38afcf64529
DELL <DELL@satya>
2022-12-04 19:23:42 +0530

    MAMATHA

---
+++ src/main/java/A8/A8/App.java
@@ -0,0 +1,18 @@
+package A8.A8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+        
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
